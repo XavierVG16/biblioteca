d2b2d4f3c0d27f6d264a2bdf376296b8d2fb567d
xavier VG <kjaviervelasque16@gmail.com>
2021-01-07 18:38:01 -0500

    first commit

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,7 +8,7 @@ import {environment} from '../../environments/environment'
   providedIn: 'root'
 })
 export class AuthService {
-  readonly URL_API = `/autenticar/`;
+  readonly URL_API = `autenticar/`;
   user = {}
   selectedUsuario: Usuario;
   usuario: Usuario[];
